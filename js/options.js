88b5357cbf93574158aab22727319b3c63142a21
Allan Legemaate <alegemaate@gmail.com>
2018-11-13 18:34:14 -0500

    Added save nonoword to settings

--- js/options.js
+++ js/options.js
@@ -1,16 +1,26 @@
-let page = document.getElementById('buttonDiv');
-const kWords = ['frick', 'frack', 'badWord', 'ohNo'];
-function constructOptions(kWords) {
-  for (let item of kWords) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.innerHTML = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('word is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
+let page = document.getElementById('wordInput');
+
+// Save Word
+function saveWord() {
+  var theValue = document.getElementById('wordToReplace').value;
+  console.log(theValue);
+  
+  chrome.storage.sync.set({'noNoWord': theValue}, 
+    function() {
+      console.log('Settings saved');
+    }
+  );
 }
-constructOptions(kWords);
+
+// Load Word
+function loadWord() {
+  chrome.storage.sync.get('noNoWord', 
+    function(result) {
+      console.log('Value currently is ' + result.noNoWord);
+      document.getElementById('wordToReplace').value = result.noNoWord;
+    }
+  );
+}
+
+document.getElementById("save").addEventListener("click", saveWord);
+document.getElementById("load").addEventListener("click", loadWord);
