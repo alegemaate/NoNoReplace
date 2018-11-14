e5713b9c7ac414078e095675fc9f896d608e128e
Allan Legemaate <alegemaate@gmail.com>
2018-11-13 20:41:35 -0500

    Censor word list implemented

--- js/options.js
+++ js/options.js
@@ -1,26 +1,72 @@
 let page = document.getElementById('wordInput');
+var wordList = null;
 
 // Save Word
 function saveWord() {
-  var theValue = document.getElementById('wordToReplace').value;
-  console.log(theValue);
+  var word = document.getElementById('wordToReplace');
+  var replacement = document.getElementById('alternativeWord');
   
-  chrome.storage.sync.set({'noNoWord': theValue}, 
+  // Add key value
+  wordList["words"][word.value] = replacement.value;
+  
+  chrome.storage.sync.set(wordList, 
     function() {
       console.log('Settings saved');
     }
   );
+  
+  word.value = "";
+  replacement.value = "";
+  
+  loadWord();
 }
 
 // Load Word
 function loadWord() {
-  chrome.storage.sync.get('noNoWord', 
+  chrome.storage.sync.get(
+    'words', 
     function(result) {
-      console.log('Value currently is ' + result.noNoWord);
-      document.getElementById('wordToReplace').value = result.noNoWord;
+      populateWords(result);
+      wordList = result;
+      
+      // If empty keep words list
+      if (wordList["words"] == null) {
+        wordList["words"] = {};
+      }
+      
+      console.log('Value currently is ' + JSON.stringify(result, null, 4));
     }
   );
 }
 
+// Populate word list
+function populateWords(words) {
+  // Get wordList div
+  var wordListDisplay = document.getElementById('wordListDisplay');
+  
+  // Check if it exists
+  if (wordListDisplay == null) {
+    console.log('wordListDisplay does not exist');
+  }
+  else {
+    // Clear it
+    wordListDisplay.innerHTML = "";
+    
+    // Fill it
+    for (var i in words["words"]) {
+      wordListDisplay.innerHTML += "<p>" + i + "</p>";
+    }
+  }
+}
+
+// Clear words
+function clearWords() {
+  chrome.storage.sync.clear();
+  loadWord();
+}
+
+loadWord();
+
 document.getElementById("save").addEventListener("click", saveWord);
 document.getElementById("load").addEventListener("click", loadWord);
+document.getElementById("clear").addEventListener("click", clearWords);
