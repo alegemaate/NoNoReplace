0ba4c91e1efe1a55a2310c81f40c16ebca8712b4
Justin Gerhardt <justin@gerhardt.link>
2018-11-13 23:17:03 -0500

    Accelerate text replacement
    fixes #1

--- js/replace.js
+++ js/replace.js
@@ -1,17 +1,14 @@
 function replaceWords() {
   chrome.storage.sync.get(
-    null, 
-    function(result) {
+    null,
+    function (result) {
       if (result["words"] != null) {
-        for (var i in result["words"]) {
-          
-          var re = new RegExp("(>[^><]*)(" + i + ")([^<>]*<)", "i"); 
-          
-          while (document.body.innerHTML.search(re) >= 0) {
-            document.body.innerHTML = document.body.innerHTML.replace(re, "$1" + result["words"][i] + "$3");
+        const iter = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+        let currentNode;
+        while (currentNode = iter.nextNode()) {
+          for (let bannedWord in result["words"]) {
+            currentNode.textContent = currentNode.textContent.replace(bannedWord, result["words"][bannedWord]);
           }
-          
-          console.log('Word ' + i + ' replaced with ' + result["words"][i] + '.');
         }
       }
     }
